5eb31b09c04b9f2dbdefe6e8bd53070a24a3e56a
MLucile <mlucile.08@gmail.com>
2018-05-26 19:42:14 +0200

    Premier visuel menu complet (non fonctionnel)

--- src/tools/Path.java
+++ src/tools/Path.java
@@ -2,4 +2,7 @@ package tools;
 
 public class Path {
     public static final String backgroundMenu = "asset/images/backg.jpg";
+
+    public static final String fontHeadCase = "../asset/fonts/HeadCase.ttf";
+    public static final String fontRemachineScript = "../asset/fonts/RemachineScript.ttf";
 }
--- src/view/ViewMenu.java
+++ src/view/ViewMenu.java
@@ -1,10 +1,21 @@
 package view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import tools.Path;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewMenu {
     // TODO VUE A TROIS BOUTONS : NOUVELLE PARTIE // CHARGER NIVEAU // QUITTER
@@ -13,12 +24,17 @@ public class ViewMenu {
 
     private Group root;
     private ImageView imgBg;
+    private Text titre;
+    private VBox menu;
+    private List<Button> listesBoutons;
+    private Button nouvellePartie, chargerNiveau, quitter;
 
     ViewMenu(Group root)
     {
         this.root = root;
         initBackground();
-        this.root.getChildren().addAll(imgBg);
+        initMenu();
+        vueMenuComplete();
     }
 
     /**
@@ -29,5 +45,56 @@ public class ViewMenu {
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getBounds(); // Récupération de la taille de l'écran
         imgBg.setFitHeight((int) primaryScreenBounds.getHeight());
         imgBg.setFitWidth((int) primaryScreenBounds.getWidth());
+    }
+
+    /**
+     * Instanciation du titre du jeu
+     */
+    private void initTitle() {
+        titre = new Text("Delirium");
+        titre.setStyle("-fx-padding: 100px;" +
+                "-fx-fill: linear-gradient(from 0% 0% to 100% 200%, repeat, #ff9f00 0%, #ecf5ac 82%);" +
+                "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.7), 10, 0.7, 0, 0);");
+
+        Font policeTitre = Font.loadFont(getClass().getResourceAsStream(Path.fontHeadCase), 110);
+        titre.setFont(policeTitre);
+    }
+
+    private void initBoutons() {
+        listesBoutons = new ArrayList<>();
+        listesBoutons.add(nouvellePartie = new Button("Nouvelle partie"));
+        listesBoutons.add(chargerNiveau = new Button("Charger un niveau"));
+        listesBoutons.add(quitter = new Button("Quitter"));
+
+        Font policeBoutons = Font.loadFont(getClass().getResourceAsStream(Path.fontRemachineScript), 60);
+        listesBoutons.forEach(button -> button.setFont(policeBoutons));
+        listesBoutons.forEach(button -> button.setTextFill(Color.DARKRED));
+        listesBoutons.forEach(button -> button.setPickOnBounds(false));
+        listesBoutons.forEach(button -> button.setBackground(null));
+
+        VBox.setMargin(nouvellePartie, new Insets((imgBg.getFitHeight() / 15), 0, 0,0));
+        /*VBox.setMargin(chargerNiveau, new Insets(0, 0,0,imgBg.getFitWidth()/2.-imgBg.getFitWidth()/6.));
+        VBox.setMargin(quitter, new Insets(0, 0,0,imgBg.getFitWidth()/2.-imgBg.getFitWidth()/9.));
+    */}
+
+    private void initMenu()
+    {
+        menu = new VBox(25);
+        menu.setAlignment(Pos.CENTER); // Les boutons sont centrés les uns par rapport aux autres
+
+        initTitle();
+        menu.getChildren().addAll(titre);
+
+        initBoutons();
+        menu.getChildren().addAll(listesBoutons);
+        menu.setPadding(new Insets(0,0,0,imgBg.getFitWidth()/2.-imgBg.getFitWidth()/5.));
+        menu.setLayoutX(50);
+        menu.setLayoutY(50);
+    }
+
+    private void vueMenuComplete()
+    {
+        root.getChildren().addAll(imgBg);
+        root.getChildren().addAll(menu);
     }
 }
