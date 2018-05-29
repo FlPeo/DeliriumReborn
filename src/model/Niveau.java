3b43826af78b30587583aa607e250b128d279ffc
Yoann HAFFNER <yoyofrom68@gmail.com>
2018-05-29 09:58:01 +0200

    niveau 1 v2 et ajout éléments pierre et vide et création classe pierre

--- src/model/Niveau.java
+++ src/model/Niveau.java
@@ -14,12 +14,14 @@ public class Niveau {
             LevelBuilder.niveau4
     };
     private static byte[][] currentLvl;
-    public final int MUR = 0;
-    public final int CLAY = 1;
-    public final int DIAMAND = 2;
-    public final int MONSTRE_BLEU = 3;
-    public final int MONSTRE_ROUGE = 4;
-    public final int MINEUR = 5;
+    public final int VIDE = 0;
+    public final int MUR = 1;
+    public final int CLAY = 2;
+    public final int DIAMAND = 3;
+    public final int PIERRE = 4;
+    public final int MONSTRE_BLEU = 5;
+    public final int MONSTRE_ROUGE = 6;
+    public final int MINEUR = 7;
     private int nbDimandToWin;
     private List<Monstre> monstreList;
     private List<Bloc> blocList;
---
+++ src/model/Pierre.java
@@ -0,0 +1,10 @@
+package model;
+
+import com.sun.javafx.geom.Vec2d;
+
+public class Pierre extends Bloc {
+
+    public Pierre(int i, int j) {
+        position = new Vec2d(i, j);
+    }
+}
