9d6d630e39751da9b128d4116141095736925def
MLucile <mlucile.08@gmail.com>
2018-05-31 23:28:06 +0200

    Premier affichage basic de la map, le zoom n'est pas encore déterminé, est-ce la bonne méthode pour l'affichage ?

--- src/tools/Path.java
+++ src/tools/Path.java
@@ -3,6 +3,20 @@ package tools;
 public class Path {
     public static final String backgroundMenu = "asset/images/backg.jpg";
 
+    public static final String background = "asset/images/background.png";
+    public static final String blueMonster = "asset/images/blue_monster.png";
+    public static final String blueMonsterAttacking = "asset/images/blue_monster_attacking.png";
+    public static final String clay = "asset/images/clay.png";
+    public static final String diamond = "asset/images/diamond.png";
+    public static final String minerDown = "asset/images/miner_down.png";
+    public static final String minerLeft = "asset/images/miner_left.png";
+    public static final String minerRight = "asset/images/miner_right.png";
+    public static final String minerUp = "asset/images/miner_up.png";
+    public static final String redMonster = "asset/images/red_monster.png";
+    public static final String redMonsterAttacking = "asset/images/red_monster_attacking.png";
+    public static final String stone = "asset/images/stone.png";
+    public static final String wall = "asset/images/wall.png";
+
     public static final String fontHeadCase = "../asset/fonts/HeadCase.ttf";
     public static final String fontRemachineScript = "../asset/fonts/RemachineScript.ttf";
 }
--- src/view/ViewGame.java
+++ src/view/ViewGame.java
@@ -2,18 +2,22 @@ package view;
 
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.image.ImageView;
 import javafx.stage.Screen;
 import model.Niveau;
 import model.Partie;
+import tools.Path;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewGame {
     //TODO GERE L'AFFICHAGE DU JEU
 
     private Group root;
     private Partie partie;
-    private GridPane grille;
+
+    private List<ImageView> images;
 
     /**
      * Constructeur de la vue du jeu
@@ -27,26 +31,74 @@ public class ViewGame {
     }
 
     private void initPlateau(){
-        Rectangle2D primaryScreenBounds = Screen.getPrimary().getBounds(); // Récupération de la taille de l'écran
+        //Rectangle2D primaryScreenBounds = Screen.getPrimary().getBounds(); // Récupération de la taille de l'écran
 
         int hauteurPlateau = Niveau.currentLvl.length;
         int largeurPlateau = Niveau.currentLvl[0].length;
+        int tailleImages = 64;/*
+        root.setScaleX(2);
+        root.setScaleY(2);*/
 
-        grille = new GridPane();
-        grille.setGridLinesVisible(true);
-        grille.setMinSize(primaryScreenBounds.getWidth()/largeurPlateau,
-                primaryScreenBounds.getHeight()/hauteurPlateau);
+        images = new ArrayList<>();
 
-        grille.add(new Button("Coucou !"),0,0);
-
-
+        for (int i = 0; i < hauteurPlateau; i++) {
+            for (int j = 0; j < largeurPlateau; j++) {
+                ImageView vide = new ImageView(Path.background);
+                vide.setTranslateX(i*tailleImages);
+                vide.setTranslateY(j*tailleImages);
+                images.add(vide);
+                switch (Niveau.currentLvl[i][j])
+                {
+                    case 1:
+                        ImageView mur = new ImageView(Path.wall);
+                        mur.setTranslateX(i*tailleImages);
+                        mur.setTranslateY(j*tailleImages);
+                        images.add(mur);
+                        break;
+                    case 2:
+                        ImageView terre = new ImageView(Path.clay);
+                        terre.setTranslateX(i*tailleImages);
+                        terre.setTranslateY(j*tailleImages);
+                        images.add(terre);
+                        break;
+                    case 3:
+                        ImageView diamond = new ImageView(Path.diamond);
+                        diamond.setTranslateX(i*tailleImages);
+                        diamond.setTranslateY(j*tailleImages);
+                        images.add(diamond);
+                        break;
+                    case 4:
+                        ImageView pierre = new ImageView(Path.stone);
+                        pierre.setTranslateX(i*tailleImages);
+                        pierre.setTranslateY(j*tailleImages);
+                        images.add(pierre);
+                        break;
+                    case 5:
+                        ImageView monstreBleu = new ImageView(Path.blueMonster);
+                        monstreBleu.setTranslateX(i*tailleImages);
+                        monstreBleu.setTranslateY(j*tailleImages);
+                        images.add(monstreBleu);
+                        break;
+                    case 6:
+                        ImageView monstreRouge = new ImageView(Path.redMonster);
+                        monstreRouge.setTranslateX(i*tailleImages);
+                        monstreRouge.setTranslateY(j*tailleImages);
+                        images.add(monstreRouge);
+                        break;
+                    case 7:
+                        ImageView mineur = new ImageView(Path.minerDown);
+                        mineur.setTranslateX(i*tailleImages);
+                        mineur.setTranslateY(j*tailleImages);
+                        images.add(mineur);
+                        break;
+                }
+            }
+        }
     }
 
     private void vueJeuComplet()
     {
         root.getChildren().clear();
-        root.getChildren().addAll(grille);
+        root.getChildren().addAll(images);
     }
-
-
 }
